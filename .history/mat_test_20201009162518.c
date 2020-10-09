47b17ed03faf781493ff5a6bd1fab827d8a5f968
Ewan Thomas <ewan.thomas98@gmail.com>
2020-10-09 17:10:02 +0100

    new branch commit

---
+++ .history/mat_test_20201009162518.c
@@ -0,0 +1,22 @@
+/* Title: Direct Matrix Calculations
+Author: Ewan-James Thomas
+Address: 35 Monkswell Road EX4 7AU
+License: Public Domain
+*/
+
+static const char * VERSION = "0.0.1";
+static const char * REV_DATE = "9-October-2020";
+
+/*
+ Date         Version  Comments
+ ----         -------  --------
+09-Oct-2020     0.0.1  Build start -> taking input from command line
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <getopt.h> /* For command line input */
+
+
+
